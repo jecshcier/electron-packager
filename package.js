db7a830804f1d588c1b55385253d7b8dab1eb856
jecshcier <alsk123111@icloud.com>
2019-03-06 14:40:05 +0800

    update

--- package.js
+++ package.js
@@ -1,18 +1,27 @@
-var packager = require('electron-packager')
+const packager = require('electron-packager')
+const path = require('path')
+const fs = require('fs-extra')
 packager({
-	'dir': '../cherry/',
-	'arch':'x64',
-	'platform':'all',
-	'appVersion':'0.0.1',
-	'out':'./electronApp',
-	'overwrite':true,
-	'download':{
-		'cache':'./electronSDK'
-	},
-	'osx-sign':true,
-	'appBundleId':'cherry',
-	'icon':'./icons/logo.icns',
-	'asar':true
-}, function done_callback(err, appPaths) { /* … */
-	console.log(err)
-})
+		'dir': '../cherry2/core',
+		'arch': 'x64',
+		'platform': 'darwin',
+		'electronVersion': '4.0.5',
+		'appVersion': '0.0.1',
+		'out': './electronApp',
+		'overwrite': true,
+		'download': {
+			'cache': './electronSDK'
+		},
+		'osx-sign': true,
+		'appBundleId': 'cherry',
+		'icon': './icons/logo.icns',
+		'asar': true
+	}).then((appPaths) => { /* … */
+		return fs.copy(path.join(__dirname, `../cherry2/app`), `${appPaths}/cherry2.app/Contents/Resources/app`)
+	})
+	.then(() => {
+		console.log('ok')
+	})
+	.catch((err) => {
+		console.log(err)
+	})
